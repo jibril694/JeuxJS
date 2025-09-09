2cf4d12449686ac4dd5ec5b60ad226702a3520b5
maksoudj <maksoudj@SNIR-P132>
2025-09-09 11:04:06 +0200

    ajout d'une section/footer/var dans le fichier html et parametrage de la reponse a une requete get dans le app.js et ajout du package express+eslint

--- JeuxJS/app.js
+++ JeuxJS/app.js
@@ -1,3 +1,29 @@
 'use strict';
 
 console.log('TP CIEL');
+
+/*  *********************** Serveur Web ***************************   */
+
+var express = require('express');
+
+//Créer une instance de l’application express:
+
+var exp = express(); 
+
+//Paramétrer le répertoire racine du site avec la méthode use:
+
+exp.use(express.static(__dirname + '/www'));
+
+exp.get('/', function (req, res) {
+    console.log(‘Reponse a un client'); 
+    res.sendFile(__dirname + '/www/index.html');
+});
+
+exp.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Erreur serveur express');
+}); 
+
+exp.listen(80, function () {
+    console.log('Serveur en ecoute');
+});
