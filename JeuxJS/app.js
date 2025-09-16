fe3e190afd836f940530c428493a01694cefa183
maksoudj <maksoudj@SNIR-P132>
2025-09-16 08:33:02 +0200

    #4 parametrage de la chaine d 'envoie(broadcast)

--- JeuxJS/app.js
+++ JeuxJS/app.js
@@ -15,7 +15,7 @@ var exp = express();
 exp.use(express.static(__dirname + '/www'));
 
 exp.get('/', function (req, res) {
-    console.log(‘Reponse a un client'); 
+    console.log('Reponse a un client'); 
     res.sendFile(__dirname + '/www/index.html');
 });
 
@@ -24,6 +24,62 @@ exp.use(function (err, req, res, next) {
     res.status(500).send('Erreur serveur express');
 }); 
 
-exp.listen(80, function () {
-    console.log('Serveur en ecoute');
+
+
+
+
+/*  *************** serveur WebSocket express *********************   */
+// 
+var expressWs = require('express-ws')(exp);
+
+// Connexion des clients à la WebSocket /echo et evenements associés 
+exp.ws('/echo', function (ws, req) {
+
+    console.log('Connection WebSocket %s sur le port %s',
+        req.connection.remoteAddress, req.connection.remotePort);
+
+    ws.on('message', function (message) {
+        console.log('De %s %s, message :%s', req.connection.remoteAddress,
+            req.connection.remotePort, message);
+        message = ws._socket._peername.address + ' : ' + message;
+        aWss.broadcast(message);
+    });
+
+    ws.on('close', function (reasonCode, description) {
+        console.log('Deconnexion WebSocket %s sur le port %s',
+            req.connection.remoteAddress, req.connection.remotePort);
+    });
+
 });
+
+
+
+
+/*  ****** Serveur web et WebSocket en ecoute sur le port 80  ********   */
+//  
+var portServ = 80;
+exp.listen(portServ, function () {
+    console.log('Serveur en ecoute');
+}); 
+
+
+
+/*  ****************** Broadcast clients WebSocket  **************   */
+var aWss = expressWs.getWss('/echo');
+var WebSocket = require('ws');
+aWss.broadcast = function broadcast(data) {
+    console.log("Broadcast aux clients navigateur : %s", data);
+    aWss.clients.forEach(function each(client) {
+        if (client.readyState == WebSocket.OPEN) {
+            client.send(data, function ack(error) {
+                console.log("    -  %s-%s", client._socket.remoteAddress,
+                    client._socket.remotePort);
+                if (error) {
+                    console.log('ERREUR websocket broadcast : %s', error.toString());
+                }
+            });
+        }
+    });
+}; 
+
+
